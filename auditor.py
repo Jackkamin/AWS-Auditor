1d8b6995ff6f16a58fc459de08cdb00c6c9d1497
Jackkamin <jackkamin201@gmail.com>
2026-06-24 01:19:53 +0100

    Initial commit: AWS auditor for EC2 and S3 status checks

---
+++ auditor.py
@@ -0,0 +1,25 @@
+import boto3
+
+s3_service = boto3.client('s3')  #specify what service we will be modifying. S3
+ec2_service = boto3.client('ec2') #specify what service we will be modifying. EC2
+
+response_s3 = s3_service.list_buckets()
+response = ec2_service.describe_instances()
+
+# checks our AWS for running/stopped services.
+for reservation in response['Reservations']: #number of servers created on creation?
+    for instance in reservation['Instances']: #instance names
+        print(instance['InstanceId'], instance['State']['Name']) #print specific entries of the dict
+
+
+#specifically looking to find our current buckets ['Buckets']
+for bucket in response_s3['Buckets']:
+    print(bucket['Name'])
+    bucket_acl = s3_service.get_bucket_acl(Bucket=bucket['Name'])
+
+    for grant in bucket_acl['Grants']:
+
+        grant_type = grant['Grantee']['Type']
+        if grant_type == 'Group':
+            print("PUBLIC")
+        else: print("PRIVATE")

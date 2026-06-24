2dba5988c72789b1c244657cefbd9b25b6eb5c41
Jackkamin <jackkamin201@gmail.com>
2026-06-24 02:09:57 +0100

    Now checks entire buckets grants and assigns if public or private. Way more efficient.

--- auditor.py
+++ auditor.py
@@ -7,19 +7,22 @@ response_s3 = s3_service.list_buckets()
 response = ec2_service.describe_instances()
 
 # checks our AWS for running/stopped services.
-for reservation in response['Reservations']: #number of servers created on creation?
+for reservation in response['Reservations']: #A list of batches that was launched on creation.?
     for instance in reservation['Instances']: #instance names
         print(instance['InstanceId'], instance['State']['Name']) #print specific entries of the dict
 
 
 #specifically looking to find our current buckets ['Buckets']
 for bucket in response_s3['Buckets']:
-    print(bucket['Name'])
     bucket_acl = s3_service.get_bucket_acl(Bucket=bucket['Name'])
+
+
+
+    is_public = False
 
     for grant in bucket_acl['Grants']:
 
         grant_type = grant['Grantee']['Type']
         if grant_type == 'Group':
-            print("PUBLIC")
-        else: print("PRIVATE")
+            is_public = True
+    print(bucket['Name'], "Is", is_public)

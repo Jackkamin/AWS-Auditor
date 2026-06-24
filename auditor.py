059d0145b0a82caff0359f07c0f308536eb9c36b
Jackkamin <jackkamin201@gmail.com>
2026-06-24 11:20:41 +0100

    Added IAM User Monitoring, Will now flag if a user access key is > 30 days old aswell as tell us important info about IAM accounts.

--- auditor.py
+++ auditor.py
@@ -1,10 +1,16 @@
 import boto3
+import datetime
+from datetime import date
 
 s3_service = boto3.client('s3')  #specify what service we will be modifying. S3
-ec2_service = boto3.client('ec2') #specify what service we will be modifying. EC2
-
 response_s3 = s3_service.list_buckets()
+
+ec2_service = boto3.client('ec2') #specify what service we will be modifying. EC2
 response = ec2_service.describe_instances()
+
+iam_service = boto3.client('iam')
+iam_users = iam_service.list_users()
+
 
 # checks our AWS for running/stopped services.
 for reservation in response['Reservations']: #A list of batches that was launched on creation.?
@@ -23,4 +29,32 @@ for bucket in response_s3['Buckets']:
         grant_type = grant['Grantee']['Type']
         if grant_type == 'Group':
             is_public = True
-    print(bucket['Name'], "Is", is_public)  #False, PRIVATE in this case.
+    print(bucket['Name'], "Is", is_public, "\n")  #False, PRIVATE in this case.
+
+
+#Ask AWS for a list of IAM users
+#Then we can ask for the age of the user and the MFA Status.
+
+print(" IAM Checks: \n")
+for users in iam_users['Users']:
+    print("User Name:", users['UserName'], "\n User ID:", users['UserId'], "\nPassword last used:", users['PasswordLastUsed'])
+    key_id = iam_service.list_access_keys(UserName=users['UserName'])['AccessKeyMetadata']
+
+    for keys in key_id: # Checks AccessKeys for the creation date and key id.
+        print("Key ID:", keys['AccessKeyId'],"\n", "Creation Date:", keys['CreateDate'])
+        current_date = datetime.datetime.now().date() #get current date, time
+        #subtracts creation date of access key from todays date, result - key age.
+        key_age = current_date - keys['CreateDate'].date()
+        print("This IAM User is: ", key_age, " Days old.")
+
+        #if key age > 30 days, flag it, if not clear.
+        if key_age.days > 30:
+            print("FLAGGED FOR REVIEW!")
+        else:
+            print("CLEAR!")
+
+
+
+
+
+
